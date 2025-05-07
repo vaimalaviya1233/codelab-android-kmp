c78cb4777f84f04b2058d90b85569237d0c6c0ff
Tomáš Mlynarič <mlykotom@google.com>
2025-05-07 12:35:41 +0200

    Revert "Add TODOs"
    This reverts commit 462b9afc193c755747baba99d289a1878787448e.

--- migrate-room/shared/build.gradle.kts
+++ migrate-room/shared/build.gradle.kts
@@ -88,10 +88,9 @@ kotlin {
 
         androidMain {
             dependencies {
-                /* TODO Add Android-specific dependencies here. Note that this source set depends on
-                    commonMain by default and will correctly pull the Android artifacts of any KMP
-                    dependencies declared in commonMain.
-                */
+                // Add Android-specific dependencies here. Note that this source set depends on
+                // commonMain by default and will correctly pull the Android artifacts of any KMP
+                // dependencies declared in commonMain.
             }
         }
 
@@ -105,12 +104,11 @@ kotlin {
 
         iosMain {
             dependencies {
-                /* TODO Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
-                    Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
-                    part of KMP’s default source set hierarchy. Note that this source set depends
-                    on common by default and will correctly pull the iOS artifacts of any
-                    KMP dependencies declared in commonMain.
-                 */
+                // Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
+                // Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
+                // part of KMP’s default source set hierarchy. Note that this source set depends
+                // on common by default and will correctly pull the iOS artifacts of any
+                // KMP dependencies declared in commonMain.
             }
         }
     }
