f099db4e39e2c9d353a212e5bfb703466df055ce
Tomáš Mlynarič <mlykotom@google.com>
2025-05-07 10:18:06 +0200

    Add TODOs

--- migrate-room/shared/build.gradle.kts
+++ migrate-room/shared/build.gradle.kts
@@ -77,7 +77,7 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
-                // Add KMP dependencies here
+                // TODO Add KMP dependencies here
                 implementation(libs.androidx.room.runtime)
                 implementation(libs.androidx.sqlite.bundled)
             }
@@ -91,9 +91,10 @@ kotlin {
 
         androidMain {
             dependencies {
-                // Add Android-specific dependencies here. Note that this source set depends on
-                // commonMain by default and will correctly pull the Android artifacts of any KMP
-                // dependencies declared in commonMain.
+                /* TODO Add Android-specific dependencies here. Note that this source set depends on
+                    commonMain by default and will correctly pull the Android artifacts of any KMP
+                    dependencies declared in commonMain.
+                */
             }
         }
 
@@ -107,16 +108,18 @@ kotlin {
 
         iosMain {
             dependencies {
-                // Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
-                // Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
-                // part of KMP’s default source set hierarchy. Note that this source set depends
-                // on common by default and will correctly pull the iOS artifacts of any
-                // KMP dependencies declared in commonMain.
+                /* TODO Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
+                    Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
+                    part of KMP’s default source set hierarchy. Note that this source set depends
+                    on common by default and will correctly pull the iOS artifacts of any
+                    KMP dependencies declared in commonMain.
+                 */
             }
         }
     }
 }
 
+// TODO Add top level dependencies block with ksp and room plugins
 dependencies {
     add("kspAndroid", libs.androidx.room.compiler)
     add("kspIosSimulatorArm64", libs.androidx.room.compiler)
@@ -124,6 +127,7 @@ dependencies {
     add("kspIosArm64", libs.androidx.room.compiler)
 }
 
+// TODO Add room configuration block
 room {
     schemaDirectory("$projectDir/schemas")
 }
